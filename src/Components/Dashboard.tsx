7e4f4a1a6c06ec3c6173687f2fe03d9195f2ac5f
Mechazzz <129221431+Mechazzz@users.noreply.github.com>
2024-01-31 11:10:42 +0100

    Adding yearFiltering for Dashboard.tsx. The total expense amount can be calculated. The most expensive activity and its data will be shown for the selected year

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -2,6 +2,7 @@ import Charts from "./Charts";
 import "../Styling/Dashboard.css";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import { UserInputType } from "../types/UserInput";
+import { newDate } from "../utils/utils";
 
 const Dashboard = () => {
   const [activities] = useLocalStorage<UserInputType[]>("activities", []);
@@ -11,16 +12,55 @@ const Dashboard = () => {
     2024
   );
 
-  const totalAmountPerYear = (year: number) => {
-    return activities
-      .filter((entry) => new Date(entry.date!).getFullYear() === year)
-      .reduce((acc, entry) => acc + entry.amount, 0);
+  const yearFilteringFunction = (year: number) => {
+    const activitiesForYear = activities.filter(
+      (entry) => new Date(entry.date!).getFullYear() === year
+    );
+    if (activitiesForYear.length === 0) {
+      return null;
+    }
+    return activitiesForYear;
   };
+
+  const totalAmountPerYear = (year: number) => {
+    const filteredYears = yearFilteringFunction(year);
+    if (filteredYears === null) {
+      return null;
+    } else {
+      const totalAmount = filteredYears.reduce(
+        (acc, entry) => acc + entry.amount,
+        0
+      );
+      return totalAmount;
+    }
+  };
+
+  const mostExpensiveActivityOfTheYear = (year: number) => {
+    const filteredActivities = yearFilteringFunction(year);
+    if (filteredActivities === null) {
+      return null;
+    }
+    const expensiveActivity = yearFilteringFunction(year)!.reduce(
+      (maxEntry, currentEntry) =>
+        currentEntry.amount > maxEntry.amount ? currentEntry : maxEntry
+    );
+    return expensiveActivity;
+  };
+
+  const mostExpensiveActivity = mostExpensiveActivityOfTheYear(selectedYear);
 
   const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const year = parseInt(event.target.value, 10);
     setSelectedYear(year);
   };
+
+  const startYear = 1950;
+  const endYear = 2100;
+
+  const years = Array.from(
+    { length: endYear - startYear + 1 },
+    (_, index) => startYear + index
+  );
 
   return (
     <>
@@ -28,16 +68,32 @@ const Dashboard = () => {
         <div>
           <label>Select year:</label>
           <select value={selectedYear} onChange={handleYearChange}>
-            <option value={2020}>2020</option>
-            <option value={2021}>2021</option>
-            <option value={2022}>2022</option>
-            <option value={2023}>2023</option>
-            <option value={2024}>2024</option>
-            <option value={2025}>2025</option>
+            {years.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
           </select>
           <p>
-            Sum for {selectedYear}: {totalAmountPerYear(selectedYear)}
+            Total expense amount in {selectedYear}:
+            {totalAmountPerYear(selectedYear)}
           </p>
+        </div>
+        <div>
+          <label>
+            {mostExpensiveActivity ? (
+              <>
+                Most expensive activity of {selectedYear} : <br />
+                Activity: {mostExpensiveActivity.activity} <br />
+                Category: {mostExpensiveActivity.category} <br />
+                Amount: {mostExpensiveActivity.amount} <br />
+                Currency: {mostExpensiveActivity.currency} <br />
+                Date: {newDate(mostExpensiveActivity.date!)} <br />
+              </>
+            ) : (
+              "No available data for the selected year"
+            )}
+          </label>
         </div>
         <div>
           <Charts />
