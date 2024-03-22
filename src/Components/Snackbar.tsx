67f5265da550347cfde4233ccd67f3252b563b9b
Mechazzz <129221431+Mechazzz@users.noreply.github.com>
2024-03-22 15:16:54 +0100

    Snackbar.css_update

--- src/Components/Snackbar.tsx
+++ src/Components/Snackbar.tsx
@@ -37,7 +37,7 @@ const Snackbar = ({ message, clearSeenMessage, offset }: SnackbarProps) => {
           }`}
         >
           {message.text}
-          <button className="xButton" onClick={handleClick}>
+          <button className="SnackbarButton" onClick={handleClick}>
             <FontAwesomeIcon icon={faXmark} />
           </button>
         </div>
